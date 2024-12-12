1a5e50835e02082ecb415b06afec258b7282d6ea
nE3ko <57717526+nE3ko@users.noreply.github.com>
2024-12-12 15:12:30 +0100

    v0.2
    Ajout :
    - Répertoire entier scannable
    - Box pour modifier une règle
    - Sauvergarder la règle (save, save as, ctrl + s)
    - Indicateur de sauvegarde
    - Box contenant la liste des fichiers trouvés

--- Program.cs
+++ Program.cs
@@ -1,73 +1,143 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 public partial class MainForm : Form
 {
+    private string _currentRulePath;
+    private Label label1;
+    private ToolStripMenuItem saveRuleAsToolStripMenuItem;
+    private Label label2;
+    private Label lbl_is_saved;
+    private string _directoryPath;
     public MainForm()
     {
         InitializeComponent();
     }
 
-    private void btnSelectRules_Click(object sender, EventArgs e)
+    private void openRuleToolStripMenuItem_Click(object sender, EventArgs e)
     {
-        OpenFileDialog openFileDialog = new OpenFileDialog();
-        openFileDialog.Title = "Sélectionnez le fichier de règles YARA";
-        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        using (OpenFileDialog ofd = new OpenFileDialog())
         {
-            txtRulesFile.Text = openFileDialog.FileName;
+            ofd.Filter = "YARA Rules (*.yar)|*.yar";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                _currentRulePath = ofd.FileName;
+                txtRuleContent.Text = File.ReadAllText(_currentRulePath);
+                lbl_is_saved.Text = "✅";
+            }
         }
     }
 
     private void btnSelectTarget_Click(object sender, EventArgs e)
     {
-        OpenFileDialog openFileDialog = new OpenFileDialog();
-        openFileDialog.Title = "Sélectionnez le fichier à analyser";
-        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        using (FolderBrowserDialog fbd = new FolderBrowserDialog())
         {
-            txtTargetFile.Text = openFileDialog.FileName;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                _directoryPath = fbd.SelectedPath;
+                txtTargetFile.Text = _directoryPath;
+            }
+        }
+    }
+
+    private void saveRuleToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(_currentRulePath))
+        {
+            File.WriteAllText(_currentRulePath, txtRuleContent.Text);
+            lbl_is_saved.Text = "✅";
+        }
+        else
+        {
+            saveRuleAsToolStripMenuItem_Click(sender, e);
+        }
+
+    }
+
+    private void saveRuleAsToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+        {
+            saveFileDialog.Filter = "Fichiers texte (*.txt)|*.txt|Tous les fichiers (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _currentRulePath = saveFileDialog.FileName;
+                File.WriteAllText(_currentRulePath, txtRuleContent.Text);
+                this.Text = $"Éditeur de texte - {Path.GetFileName(_currentRulePath)}";
+                MessageBox.Show("Fichier sauvegardé avec succès.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lbl_is_saved.Text = "✅";
+            }
+        }
+
+    }
+
+    private void MainForm_KeyDown(object sender, KeyEventArgs e)
+    {
+        // Vérifie si Ctrl + S est pressé
+        if (e.Control && e.KeyCode == Keys.S)
+        {
+            saveRuleToolStripMenuItem_Click(sender, e);
         }
     }
 
     private void btnAnalyze_Click(object sender, EventArgs e)
     {
         string yaraExe = "yara64.exe";
-        string rulesFile = txtRulesFile.Text;
-        string targetFile = txtTargetFile.Text;
+        string rulesFile = _currentRulePath;
+        string targetDirectory = _directoryPath;
 
-        if (!System.IO.File.Exists(yaraExe) || !System.IO.File.Exists(rulesFile) || !System.IO.File.Exists(targetFile))
+        if (!System.IO.File.Exists(yaraExe) || !System.IO.File.Exists(rulesFile) || !System.IO.Directory.Exists(targetDirectory))
         {
-            MessageBox.Show("Un ou plusieurs fichiers sont introuvables.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show("Le fichier YARA, le fichier de règles ou le répertoire cible est introuvable.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
             return;
         }
 
-        string arguments = $"{rulesFile} {targetFile}";
         try
         {
-            var process = new Process
+            // Effacer les résultats précédents
+            lstResults.Items.Clear();
+
+            // Obtenir tous les fichiers dans le répertoire
+            string[] files = Directory.GetFiles(targetDirectory, "*.*", SearchOption.AllDirectories);
+
+            foreach (string targetFile in files)
             {
-                StartInfo = new ProcessStartInfo
+                string arguments = $"{rulesFile} {targetFile}";
+
+                var process = new Process
                 {
-                    FileName = yaraExe,
-                    Arguments = arguments,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = yaraExe,
+                        Arguments = arguments,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                string errors = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(output))
+                {
+                    lstResults.Items.Add($"{Path.GetFileName(targetFile)}");
                 }
-            };
+                else if (!string.IsNullOrEmpty(errors))
+                {
+                    lstResults.Items.Add($"{Path.GetFileName(targetFile)}");
+                }
+            }
 
-            process.Start();
-            string output = process.StandardOutput.ReadToEnd();
-            string errors = process.StandardError.ReadToEnd();
-            process.WaitForExit();
-
-            if (!string.IsNullOrEmpty(output))
-                MessageBox.Show(output, "Résultat", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            else if (!string.IsNullOrEmpty(errors))
-                MessageBox.Show(errors, "Erreurs", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            else
-                MessageBox.Show("Aucun résultat trouvé.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (lstResults.Items.Count == 0)
+            {
+                lstResults.Items.Add("Aucun résultat trouvé.");
+            }
         }
         catch (Exception ex)
         {
@@ -75,84 +145,196 @@ public partial class MainForm : Form
         }
     }
 
+
     private void InitializeComponent()
     {
-        this.btnSelectRules = new System.Windows.Forms.Button();
-        this.btnSelectTarget = new System.Windows.Forms.Button();
-        this.txtRulesFile = new System.Windows.Forms.TextBox();
-        this.txtTargetFile = new System.Windows.Forms.TextBox();
-        this.btnAnalyze = new System.Windows.Forms.Button();
-        this.SuspendLayout();
-
-        // 
-        // btnSelectRules
-        // 
-        this.btnSelectRules.Location = new System.Drawing.Point(12, 12);
-        this.btnSelectRules.Name = "btnSelectRules";
-        this.btnSelectRules.Size = new System.Drawing.Size(200, 23);
-        this.btnSelectRules.TabIndex = 0;
-        this.btnSelectRules.Text = "Sélectionner le fichier de règles YARA";
-        this.btnSelectRules.UseVisualStyleBackColor = true;
-        this.btnSelectRules.Click += new System.EventHandler(this.btnSelectRules_Click);
-
+        btnSelectTarget = new System.Windows.Forms.Button();
+        txtTargetFile = new System.Windows.Forms.TextBox();
+        btnAnalyze = new System.Windows.Forms.Button();
+        txtRuleContent = new System.Windows.Forms.TextBox();
+        menuStrip1 = new MenuStrip();
+        fIleToolStripMenuItem = new ToolStripMenuItem();
+        openRuleToolStripMenuItem = new ToolStripMenuItem();
+        saveRuleToolStripMenuItem = new ToolStripMenuItem();
+        saveRuleAsToolStripMenuItem = new ToolStripMenuItem();
+        lstResults = new ListBox();
+        label1 = new Label();
+        label2 = new Label();
+        lbl_is_saved = new Label();
+        menuStrip1.SuspendLayout();
+        SuspendLayout();
         // 
         // btnSelectTarget
         // 
-        this.btnSelectTarget.Location = new System.Drawing.Point(12, 41);
-        this.btnSelectTarget.Name = "btnSelectTarget";
-        this.btnSelectTarget.Size = new System.Drawing.Size(200, 23);
-        this.btnSelectTarget.TabIndex = 1;
-        this.btnSelectTarget.Text = "Sélectionner le fichier à analyser";
-        this.btnSelectTarget.UseVisualStyleBackColor = true;
-        this.btnSelectTarget.Click += new System.EventHandler(this.btnSelectTarget_Click);
-
-        // 
-        // txtRulesFile
-        // 
-        this.txtRulesFile.Location = new System.Drawing.Point(218, 14);
-        this.txtRulesFile.Name = "txtRulesFile";
-        this.txtRulesFile.Size = new System.Drawing.Size(300, 20);
-        this.txtRulesFile.TabIndex = 2;
-
+        btnSelectTarget.Location = new Point(14, 284);
+        btnSelectTarget.Name = "btnSelectTarget";
+        btnSelectTarget.Size = new Size(124, 27);
+        btnSelectTarget.TabIndex = 1;
+        btnSelectTarget.Text = "Select directory";
+        btnSelectTarget.UseVisualStyleBackColor = true;
+        btnSelectTarget.Click += btnSelectTarget_Click;
         // 
         // txtTargetFile
         // 
-        this.txtTargetFile.Location = new System.Drawing.Point(218, 43);
-        this.txtTargetFile.Name = "txtTargetFile";
-        this.txtTargetFile.Size = new System.Drawing.Size(300, 20);
-        this.txtTargetFile.TabIndex = 3;
-
+        txtTargetFile.Location = new Point(154, 284);
+        txtTargetFile.Name = "txtTargetFile";
+        txtTargetFile.Size = new Size(300, 27);
+        txtTargetFile.TabIndex = 3;
         // 
         // btnAnalyze
         // 
-        this.btnAnalyze.Location = new System.Drawing.Point(12, 70);
-        this.btnAnalyze.Name = "btnAnalyze";
-        this.btnAnalyze.Size = new System.Drawing.Size(506, 23);
-        this.btnAnalyze.TabIndex = 4;
-        this.btnAnalyze.Text = "Analyser";
-        this.btnAnalyze.UseVisualStyleBackColor = true;
-        this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
-
+        btnAnalyze.Location = new Point(14, 317);
+        btnAnalyze.Name = "btnAnalyze";
+        btnAnalyze.Size = new Size(440, 34);
+        btnAnalyze.TabIndex = 4;
+        btnAnalyze.Text = "Analyser";
+        btnAnalyze.UseVisualStyleBackColor = true;
+        btnAnalyze.Click += btnAnalyze_Click;
+        // 
+        // txtRuleContent
+        // 
+        txtRuleContent.Dock = DockStyle.Top;
+        txtRuleContent.Font = new Font("Consolas", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        txtRuleContent.Location = new Point(0, 28);
+        txtRuleContent.Multiline = true;
+        txtRuleContent.Name = "txtRuleContent";
+        txtRuleContent.ScrollBars = ScrollBars.Both;
+        txtRuleContent.Size = new Size(882, 154);
+        txtRuleContent.TabIndex = 5;
+        txtRuleContent.TextChanged += txtRuleContent_TextChanged;
+        // 
+        // menuStrip1
+        // 
+        menuStrip1.ImageScalingSize = new Size(20, 20);
+        menuStrip1.Items.AddRange(new ToolStripItem[] { fIleToolStripMenuItem });
+        menuStrip1.Location = new Point(0, 0);
+        menuStrip1.Name = "menuStrip1";
+        menuStrip1.Size = new Size(882, 28);
+        menuStrip1.TabIndex = 6;
+        menuStrip1.Text = "menuStrip1";
+        // 
+        // fIleToolStripMenuItem
+        // 
+        fIleToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openRuleToolStripMenuItem, saveRuleToolStripMenuItem, saveRuleAsToolStripMenuItem });
+        fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+        fIleToolStripMenuItem.Size = new Size(52, 24);
+        fIleToolStripMenuItem.Text = "Rule";
+        fIleToolStripMenuItem.Click += fIleToolStripMenuItem_Click;
+        // 
+        // openRuleToolStripMenuItem
+        // 
+        openRuleToolStripMenuItem.Name = "openRuleToolStripMenuItem";
+        openRuleToolStripMenuItem.Size = new Size(141, 26);
+        openRuleToolStripMenuItem.Text = "Open";
+        openRuleToolStripMenuItem.Click += openRuleToolStripMenuItem_Click;
+        // 
+        // saveRuleToolStripMenuItem
+        // 
+        saveRuleToolStripMenuItem.Name = "saveRuleToolStripMenuItem";
+        saveRuleToolStripMenuItem.Size = new Size(141, 26);
+        saveRuleToolStripMenuItem.Text = "Save";
+        saveRuleToolStripMenuItem.Click += saveRuleToolStripMenuItem_Click;
+        // 
+        // saveRuleAsToolStripMenuItem
+        // 
+        saveRuleAsToolStripMenuItem.Name = "saveRuleAsToolStripMenuItem";
+        saveRuleAsToolStripMenuItem.Size = new Size(141, 26);
+        saveRuleAsToolStripMenuItem.Text = "Save as";
+        saveRuleAsToolStripMenuItem.Click += saveRuleAsToolStripMenuItem_Click;
+        // 
+        // lstResults
+        // 
+        lstResults.AccessibleName = "";
+        lstResults.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        lstResults.FormattingEnabled = true;
+        lstResults.Location = new Point(507, 224);
+        lstResults.Name = "lstResults";
+        lstResults.Size = new Size(350, 164);
+        lstResults.TabIndex = 7;
+        lstResults.SelectedIndexChanged += lstResults_SelectedIndexChanged;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(507, 201);
+        label1.Name = "label1";
+        label1.Size = new Size(108, 20);
+        label1.TabIndex = 8;
+        label1.Text = "Detected files :";
+        label1.Click += label1_Click;
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(15, 189);
+        label2.Name = "label2";
+        label2.Size = new Size(56, 20);
+        label2.TabIndex = 9;
+        label2.Text = "Saved :";
+        // 
+        // lbl_is_saved
+        // 
+        lbl_is_saved.AutoSize = true;
+        lbl_is_saved.Location = new Point(77, 189);
+        lbl_is_saved.Name = "lbl_is_saved";
+        lbl_is_saved.Size = new Size(0, 20);
+        lbl_is_saved.TabIndex = 10;
         // 
         // MainForm
         // 
-        this.ClientSize = new System.Drawing.Size(530, 105);
-        this.Controls.Add(this.btnAnalyze);
-        this.Controls.Add(this.txtTargetFile);
-        this.Controls.Add(this.txtRulesFile);
-        this.Controls.Add(this.btnSelectTarget);
-        this.Controls.Add(this.btnSelectRules);
-        this.Name = "MainForm";
-        this.Text = "Analyseur YARA";
-        this.ResumeLayout(false);
-        this.PerformLayout();
+        ClientSize = new Size(882, 446);
+        Controls.Add(lbl_is_saved);
+        Controls.Add(label2);
+        Controls.Add(label1);
+        Controls.Add(lstResults);
+        Controls.Add(txtRuleContent);
+        Controls.Add(btnAnalyze);
+        Controls.Add(txtTargetFile);
+        Controls.Add(btnSelectTarget);
+        Controls.Add(menuStrip1);
+        MainMenuStrip = menuStrip1;
+        Name = "MainForm";
+        Text = "Analyseur YARA";
+        menuStrip1.ResumeLayout(false);
+        menuStrip1.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
+        //
+        // Key ctrl + s
+        //
+        this.KeyPreview = true;   
+        this.KeyDown += MainForm_KeyDown;
     }
 
-    private System.Windows.Forms.Button btnSelectRules;
     private System.Windows.Forms.Button btnSelectTarget;
-    private System.Windows.Forms.TextBox txtRulesFile;
     private System.Windows.Forms.TextBox txtTargetFile;
+    private System.Windows.Forms.TextBox txtRuleContent;
+    private MenuStrip menuStrip1;
+    private ToolStripMenuItem fIleToolStripMenuItem;
+    private ToolStripMenuItem openRuleToolStripMenuItem;
+    private ToolStripMenuItem saveRuleToolStripMenuItem;
     private System.Windows.Forms.Button btnAnalyze;
+    private ListBox lstResults;
+
+    private void lstResults_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void fIleToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void txtRuleContent_TextChanged(object sender, EventArgs e)
+    {
+        lbl_is_saved.Text = "❌";
+    }
 }
 
 public static class Program
